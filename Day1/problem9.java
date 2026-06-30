b2473f195ea651614b3c5d806d13bf8fff9f4bae
vithyaseenivasan2-pixel <vithyaseenivasan2@gmail.com>
2026-06-30 23:04:49 +0530

    Initial

---
+++ Day1/problem9.java
@@ -0,0 +1,9 @@
+package Day1;
+public class problem9 {
+     String name = "Rupana";
+    public static void main(String[] args) {
+       problem9 s = new problem9();
+        System.out.println(s.name);
+    }
+}
+
