e974e9f105518a6865a37e2a738d7665df1fd010
vithyaseenivasan2-pixel <vithyaseenivasan2@gmail.com>
2026-06-30 23:23:48 +0530

    Initial

---
+++ Day1/problem14.java
@@ -0,0 +1,8 @@
+package Day1;
+public class problem14 {
+    public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+        System.out.println("Processors: " + r.availableProcessors());
+    }
+}
+
