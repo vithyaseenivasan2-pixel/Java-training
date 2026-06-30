01d7fd54932d590d05e52ee95cc3c8531a0f1179
vithyaseenivasan2-pixel <vithyaseenivasan2@gmail.com>
2026-06-30 23:18:32 +0530

    Initial

---
+++ Day1/problem12.java
@@ -0,0 +1,10 @@
+package Day1;
+public class problem12 {
+     static {
+        System.out.println("Static Block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
+
