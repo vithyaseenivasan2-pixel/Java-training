33e2c451ae6ec6e06eaca13539b29a1db23a5379
vithyaseenivasan2-pixel <vithyaseenivasan2@gmail.com>
2026-06-30 23:08:21 +0530

    Initial

---
+++ Day1/problem10.java
@@ -0,0 +1,10 @@
+package Day1;
+public class problem10 {
+    void show() {
+        System.out.println("Method Called");
+    }
+    public static void main(String[] args) {
+        new problem10().show();
+    }
+}
+
