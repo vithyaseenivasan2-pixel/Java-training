2f31a2f9cd74de9272d79791bda22f27f2e3e755
vithyaseenivasan2-pixel <vithyaseenivasan2@gmail.com>
2026-06-30 23:15:36 +0530

    Initial

---
+++ Day1/problem11.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class problem11 {
+    problem11() {
+        System.out.println("Constructor Executed");
+    }
+    public static void main(String[] args) {
+        new problem11();
+    }
+}
+
